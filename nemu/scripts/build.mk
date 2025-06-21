8084dd7b8e245cf481659e3c96e52833e35cdea2
2401280000-Zou Ziqi <473878330@qq.com>
2025-06-21 22:32:10 +0800

    Calculate the number of file lines of the pa0

--- nemu/scripts/build.mk
+++ nemu/scripts/build.mk
@@ -45,7 +45,7 @@ $(OBJ_DIR)/%.o: %.cc
 
 # Some convenient rules
 
-.PHONY: app clean
+.PHONY: app clean count
 
 app: $(BINARY)
 
@@ -55,3 +55,6 @@ $(BINARY):: $(OBJS) $(ARCHIVES)
 
 clean:
 	-rm -rf $(BUILD_DIR)
+	
+count:
+	./count.sh
