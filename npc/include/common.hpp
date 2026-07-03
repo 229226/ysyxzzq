90b36ca14650bb94941eed6e60dcf9e46a1aeaae
2401280000-Zou Ziqi <473878330@qq.com>
2026-07-03 15:05:18 +0800

    通过中断等待SPI master传输完成

--- npc/include/common.hpp
+++ npc/include/common.hpp
@@ -12,9 +12,9 @@
 #include <string.h>
 
 //#define DIFF_CONFIG
-#define TRACE_CONFIG
-#define ITRACE_CONFIG
+//#define TRACE_CONFIG
+//#define ITRACE_CONFIG
 //#define MTRACE_CONFIG
-#define FST_CONFIG
+//#define FST_CONFIG
 
 #endif
