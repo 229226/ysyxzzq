19d67f0566eb3f41d083759b62279e11b8212e9a
2401280000-Zou Ziqi <473878330@qq.com>
2026-07-16 10:18:04 +0800

    对SDRAM控制器进行字扩展

--- npc/csrc/io/sdram.cpp
+++ npc/csrc/io/sdram.cpp
@@ -1,13 +1,14 @@
 #include "sdram.hpp"
 #include "trace.hpp"
 
-static uint8_t sdram [2][SDRAM_SIZE];
+static uint8_t sdram [4][SDRAM_SIZE];
 
 extern "C" void sdram_read(uint32_t addr, uint8_t *data, uint8_t number) {
     *data = *((uint8_t *)((uint64_t)(sdram[number]) + addr));
     #ifdef MTRACE_CONFIG
         mtrace_read(SDRAM_ADDR_BASE + 
-            ( ((addr&0xfffffffe) << 1) | (addr&0x00000001) | ((uint32_t)number << 1) )
+            ( ((addr&0xfffffffe) << 1) | (addr&0x00000001) | 
+            ((uint32_t)(number&0x01) << 1) | ((uint32_t)(number&0x02) << 25) )
             ,*data,1);
     #endif
 }
@@ -16,7 +17,8 @@ extern "C" void sdram_write(uint32_t addr, uint8_t data, uint8_t number) {
     *((uint8_t *)((uint64_t)(sdram[number]) + addr)) = data;
     #ifdef MTRACE_CONFIG
         mtrace_write(SDRAM_ADDR_BASE +
-            ( ((addr&0xfffffffe) << 1) | (addr&0x00000001) | ((uint32_t)number << 1) )
+            ( ((addr&0xfffffffe) << 1) | (addr&0x00000001) | 
+            ((uint32_t)(number&0x01) << 1) | ((uint32_t)(number&0x02) << 25))
             ,data,1);
     #endif
 }
